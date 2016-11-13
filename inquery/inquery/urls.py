a4403f7be8536e80f351b466f8c1749be1870260
Paco Huerta <paco.huerta1218@gmail.com>
2016-11-13 04:25:07 -0600

    alexa.py file and urls added

--- inquery/inquery/urls.py
+++ inquery/inquery/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from . import views
 
 
 urlpatterns = [
+    url(r'^', include('django_alexa.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', auth_views.login, name='login', kwargs={'redirect_authenticated_user': True}),
     url(r'^login/$', auth_views.login, name='login', kwargs={'redirect_authenticated_user': True}),
