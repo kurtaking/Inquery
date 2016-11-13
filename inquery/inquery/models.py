bd2906dc17b899572fc260642449a4c58abca54f
Kurt King <kurtaking@gmail.com>
2016-11-12 20:11:02 -0600

    Added models needed for inquery

--- inquery/inquery/models.py
+++ inquery/inquery/models.py
@@ -1,2 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User as AuthUser
+
+class Question(models.Model):
+    question = models.CharField(max_length=200)
+
+
+class Answer(models.Model):
+    answer = models.CharField(max_length=1000)
+
+
+class FlashCard(models.Model):
+    question = models.ManyToManyField(Question)
+    answer = models.ManyToManyField(Answer)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    flash_cards = models.ManyToManyField(FlashCard)
+
+
+class User(AuthUser):
+    categories = models.ManyToManyField(Category)
+
